8e4e10ca200c472d7df6ab1dfdf5d2ae24dec7dd
poora12 <rtiact2005india1947@gmail.com>
2020-06-06 07:56:08 +0530

    hey

--- p2.cpp
+++ p2.cpp
@@ -4,16 +4,19 @@ using namespace std;
 int main()
 {
     int a = 5, b = 10, n;
-    cout << "give the value of numer";
+    cout << "give the value of numer \n \t ";
     cin >> n;
     if (n > a)
     {
-        cout << "greater than 5";
+        cout << " given number is greater than 5 \n "
+             << "good luck \t " << endl;
     }
     else
     {
-        cout << "lower than 5";
+        cout << "given number is lower than 5 \n "
+             << "good luck \t " << endl;
     }
 
-    return 0;
+    system("pause");
+    // return 0;
 }
